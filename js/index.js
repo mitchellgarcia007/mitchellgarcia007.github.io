ff99a31d061843bae7eb31c7ee2afbb346d4bc55
mitchellgarcia007 <ca103mitchellgarcia@yahoo.com>
2020-02-05 00:04:56 -0800

    organized code and  centered room

--- js/index.js
+++ js/index.js
@@ -1,44 +1,40 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
     $("nav").hide();
-	$("#mainmain").fadeIn(3000);
+    $("#mainmain").fadeIn(3000);
 
-	$(".spanish").hide();
-	
+    $(".spanish").hide();
 
-	$(".toSpanish").click(function(){
-		$(".english").hide();
+    $(".toSpanish").click(function() {
+        $(".english").hide();
         $(".spanish").show();
     });
 
-	$(".toEnglish").click(function(){
-		$(".spanish").hide();
+    $(".toEnglish").click(function() {
+        $(".spanish").hide();
         $(".english").show();
     });
 
-	$("#menu_mobile").click(function(){
-		$("#menu_mobile").hide();	
-		$("#menu_mobile_close").css("display", "block");
-		$("nav").slideDown();
+    $("#menu_mobile").click(function() {
+        $("#menu_mobile").hide();
+        $("#menu_mobile_close").css("display", "block");
+        $("nav").slideDown();
     });
 
-	$("#menu_mobile_close").click(function(){
-		$("#menu_mobile_close").hide();
-		$("#menu_mobile").show();	
-		$("nav").slideUp();
+    $("#menu_mobile_close").click(function() {
+        $("#menu_mobile_close").hide();
+        $("#menu_mobile").show();
+        $("nav").slideUp();
     });
 
-	$(".toSpanish_mobile").click(function(){
-		$("#menu_mobile_close").hide();
-		$("#menu_mobile").show();
-		$("nav").slideUp();
-	});
+    $(".toSpanish_mobile").click(function() {
+        $("#menu_mobile_close").hide();
+        $("#menu_mobile").show();
+        $("nav").slideUp();
+    });
 
-	$(".toEnglish_mobile").click(function(){
-		$("#menu_mobile_close").hide();
-		$("#menu_mobile").show();
-		$("nav").slideUp();
-	});
-
-
+    $(".toEnglish_mobile").click(function() {
+        $("#menu_mobile_close").hide();
+        $("#menu_mobile").show();
+        $("nav").slideUp();
+    });
 });
